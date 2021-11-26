d7ef1d4d6882e2032a31140cc2d2bea0a4c88103
Richard Forrest <feanz@users.noreply.github.com>
2021-11-26 10:47:44 +0700

    Update title code

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -58,7 +58,7 @@ def index():
         vote2 = r.get(button2).decode('utf-8')            
 
         # Return index with values
-        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title='Richards AKS')
+        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
     elif request.method == 'POST':
 
